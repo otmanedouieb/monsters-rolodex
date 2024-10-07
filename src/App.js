b069162a4ed258732e87d60559c7e07404da782a
otmane douieb <douieb197@gmail.com>
2024-10-07 22:47:59 +0000

    Create app with class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    console.log("constructor: App");
+    super();
+
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount: App");
+
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(() => {
+          return {
+            monsters: users,
+            searchValue: "",
+          };
+        })
+      );
+  }
+
+  searchHandler = (event) => {
+    const value = event.target.value.toLocaleLowerCase();
+
+    this.setState(() => {
+      return {
+        searchValue: value,
+      };
+    });
+  };
+
+  render() {
+    console.log("render: App");
+    console.log(this.state);
+
+    const filteredMonsters = this.state.monsters.filter((monster) =>
+      monster.name.toLocaleLowerCase().includes(this.state.searchValue)
+    );
+
+    return (
+      <div>
+        <input type="search" onChange={this.searchHandler} />
+
+        {filteredMonsters.map((monster) => (
+          <h1 key={monster.id}>{monster.name}</h1>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
